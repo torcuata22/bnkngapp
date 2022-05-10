c606abea6bbe8d219fa4edb8c50dce12e46dcbf4
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-09 20:32:57 -0400

    added test code for User

--- banking.py
+++ banking.py
@@ -3,3 +3,16 @@ class User:
         self.name = name
         self.pin = pin
         self.password = password
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# Test code for User:
+#user1 = User("Bob", "1234", "password")
+#print(user1.name, user1.pin, user1.password)
